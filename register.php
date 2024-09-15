d80cd13203c9631abc36943afa7be81e36fa26b2
Petar Turanovic <petarturanovic00@gmail.com>
2024-09-15 23:07:40 +0200

    Adding styling for register.php

--- register.php
+++ register.php
@@ -22,21 +22,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql = "INSERT INTO users (username, password, weight) VALUES ('$username', '$pass', '$weight')";
 
     if ($conn->query($sql) === TRUE) {
-        echo "Registration successful!";
+        $message = "Registration successful!";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        $message = "Error: " . $sql . "<br>" . $conn->error;
     }
 }
 $conn->close();
 ?>
 
 <!-- HTML REGISTER FORM -->
-<form method="POST" action="register.php">
-    <label for="username">Username:</label><br>
-    <input type="text" id="username" name="username" required><br><br>
-    <label for="password">Password:</label><br>
-    <input type="password" id="password" name="password" required><br><br>
-    <label for="email">Weight:</label><br>
-    <input type="text" id="weight" name="weight" required><br><br>
-    <button type="submit">Register</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>FitQuest - Register</title>
+    <link rel="stylesheet" href="css/register.css" />
+</head>
+
+<body>
+    <header>
+        <h1><span class="fit">Fit</span><span class="quest">Quest</span></h1>
+    </header>
+    <form method="POST" action="register.php">
+        <label for="username">Username:</label><br>
+        <input type="text" id="username" name="username" required><br><br>
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+        <label for="email">Weight:</label><br>
+        <input type="text" id="weight" name="weight" required><br><br>
+        <a href="login.php" class="login-link"><span>Log in</span></a>
+        <button type="submit">Register</button>
+    </form>
+    <?php if (!empty($message)): ?>
+        <div class="message">
+            <?php echo $message; ?>
+        </div>
+    <?php endif; ?>
+</body>
+
+</html>
